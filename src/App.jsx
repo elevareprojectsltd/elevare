d4b8690aae5f9dab8cfc6520f3e7c5d1cc6dd886
Tifeh13 <afolabibolu15@gmail.com>
2026-04-07 14:07:40 +0100

    Add Google Analytics tracking

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import ReactGA from "react-ga4";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Contact from "./pages/Contact"
-import Construction from "./pages/Construction"
-import Technology from "./pages/Technology"
+import Contact from "./pages/Contact";
+import Construction from "./pages/Construction";
+import Technology from "./pages/Technology";
 import Portfolio from "./pages/Portfolio";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 
+// Initialize GA once when the app loads
+ReactGA.initialize(import.meta.env.VITE_GA_ID);
 
 function App() {
+  const location = useLocation();
+
+  // Fire a pageview every time the route changes
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
+
   return (
     <div
       className="min-h-screen transition-colors duration-300"
@@ -23,12 +34,10 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/contact" element={<Contact />} />
         <Route path="/services/construction" element={<Construction />} />
         <Route path="/services/technology" element={<Technology />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/privacy" element={<PrivacyPolicy />} />
-
       </Routes>
     </div>
   );
